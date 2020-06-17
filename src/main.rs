4868fa8ae01cfe393c15702fb75ea0407d13b43d
Deepankar Agrawal <deepankar11093@gmail.com>
2020-06-17 20:16:10 +0530

    add image write

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate image;
+
+use image::{ImageBuffer, RgbImage};
+
 fn main() {
-    println!("Hello, world!");
+    // Construct a new RGB ImageBuffer with the specified width and height.
+    let mut img: RgbImage = ImageBuffer::new(512, 512);
+
+    // Put a pixel at coordinate (100, 100).
+    img.put_pixel(100, 100, image::Rgb([255, 0, 0]));
+
+    img.save("output.bmp").unwrap();
 }
