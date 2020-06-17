7835864912d5a562b3368ff0f0b81b510991f3b1
Deepankar Agrawal <deepankar11093@gmail.com>
2020-06-18 02:11:34 +0530

    optimize draw line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![feature(test)]
+extern crate test;
+use test::Bencher;
+
 extern crate image;
 
 use image::{imageops, ImageBuffer, RgbImage};
@@ -9,9 +13,9 @@ fn main() {
     let white = image::Rgb([255, 255, 255]);
     let green = image::Rgb([0, 255, 0]);
 
-    draw_line(13, 20, 100, 60, &mut img, red);
-    draw_line(20, 13, 40, 80, &mut img, white);
-    draw_line(80, 40, 13, 20, &mut img, green);
+    draw_line(13, 20, 100, 60, &mut img, &red);
+    draw_line(20, 13, 40, 80, &mut img, &white);
+    draw_line(80, 40, 13, 20, &mut img, &green);
 
     // flip vertically to make origin at bottom left
     imageops::flip_vertical_in_place(&mut img);
@@ -24,7 +28,7 @@ fn draw_line(
     mut x2: i32,
     mut y2: i32,
     image: &mut RgbImage,
-    color: image::Rgb<u8>,
+    color: &image::Rgb<u8>,
 ) {
     let mut steep = false;
     if ((x1 - x2) as i32).abs() < ((y1 - y2) as i32).abs() {
@@ -36,13 +40,29 @@ fn draw_line(
         std::mem::swap(&mut x1, &mut x2);
         std::mem::swap(&mut y1, &mut y2);
     }
+    let dx = x2 - x1;
+    let dy = y2 - y1;
+    let derror = (dy * 2).abs();
+    let mut error = 0;
+    let mut y = y1;
     for x in x1..x2 {
-        let t = (x - x1) as f32 / (x2 - x1) as f32;
-        let y = y1 as f32 * t + ((1f32 - t) * y2 as f32);
         if steep {
-            image.put_pixel(y as u32, x as u32, color);
+            image.put_pixel(y as u32, x as u32, *color);
         } else {
-            image.put_pixel(x as u32, y as u32, color);
+            image.put_pixel(x as u32, y as u32, *color);
+        }
+        error += derror;
+        if error > dx {
+            y += if y2 > y1 { 1 } else { -1 };
+            error -= dx * 2;
         }
     }
+}
+
+#[bench]
+fn bench_draw_line(b: &mut Bencher) {
+    let mut img: RgbImage = ImageBuffer::new(256, 256);
+
+    let red = image::Rgb([255, 0, 0]);
+    b.iter(|| draw_line(30, 30, 10, 10, &mut img, &red));
 }
